15a8814b412fd01d262307a8c5fcf4bf7c6f6e40
Kush <97338412+KushBharakhada@users.noreply.github.com>
2023-01-20 16:30:08 +0000

    Call back function.

--- perceptron.py
+++ perceptron.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 class Perceptron(object):
-    def __init__(self):
+    def __init__(self, gui_update_callback):
         self.__w = np.array([1.0, -1.0, 1.0])
         self.__p = 0.5
         self.__train = np.array([
             [[0, 1, 1], [1, 0, 1]],
             [[1, 3, 1], [3, 0, 1]]
         ])
+        self.__callback = gui_update_callback
 
     def set_learning_rate(self, p):
         self.__p = p
@@ -22,30 +23,31 @@ class Perceptron(object):
         class1_points, class1 = self.__train[0], 1
         class2_points, class2 = self.__train[1], -1
         finished = False
-        print("here")
 
         while not finished:
             misclassified_points = []
             for positive_point in class1_points:
-                wx = self.__w@positive_point
+                wx = self.__w @ positive_point
                 if (class2 * wx) >= 0:
-                    print(f"Misclassified {positive_point} as class 2. Output: {wx}")
                     misclassified_points.append(positive_point * class2)
 
             for negative_point in class2_points:
-                wx = self.__w@negative_point
+                wx = self.__w @ negative_point
                 if (class1 * wx) >= 0:
-                    print(f"Misclassified {negative_point} as class 1. Output: {wx}")
                     misclassified_points.append(negative_point * class1)
 
             if len(misclassified_points) == 0:
                 finished = True
 
-            print(np.sum(np.array(misclassified_points), axis=0))
             self.__w = self.__w - self.__p * np.sum(np.array(misclassified_points), axis=0)
 
-            print(f"Updated weights. New weights: {self.__w}")
+            self.__callback(self.__w)
+
+
+def test_callback(weights):
+    print(weights)
+
 
 if __name__ == "__main__":
-    perceptron = Perceptron()
+    perceptron = Perceptron(test_callback)
     perceptron.run_perceptron()
